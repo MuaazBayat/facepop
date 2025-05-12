e6185bcc426f29e49ada5f1050c82911028e9be0
Muaaz <muaazbayat@gmail.com>
2025-05-12 22:55:52 +0200

    removed confgi' git push '

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
 // vite.config.js
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   optimizeDeps: {
-    plugins: [react()],
     base: '/facepop/',
     include: ['@gomomento/sdk-web']
   },
