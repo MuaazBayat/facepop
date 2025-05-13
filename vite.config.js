8a48bafe24fc3aa8f26c15d5a560722c40556b49
Muaaz <muaazbayat@gmail.com>
2025-05-13 09:16:18 +0200

    one last time

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,6 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  optimizeDeps: {
-    base: '/facepop/',
-    include: ['@gomomento/sdk-web']
-  },
+  base: '/facepop/',
+  include: ['@gomomento/sdk-web']
 });
